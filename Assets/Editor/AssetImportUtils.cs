a151e6a821f819176929833411e3f217624d8774
SaltyFish <1048282883@qq.com>
2024-11-17 18:39:24 +0800

    资源导入

---
+++ Assets/Editor/AssetImportUtils.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEditor;
+public static class AssetImportUtils
+{
+    private static bool isStartAssetEditing = false;
+    public static void StartAssetEditing()
+    {
+        if (!isStartAssetEditing)
+        {
+            AssetDatabase.StartAssetEditing();
+            isStartAssetEditing = true;
+        }
+    }
+    public static void StopAssetEditing()
+    {
+        if (isStartAssetEditing)
+        {
+            AssetDatabase.StopAssetEditing();
+            isStartAssetEditing = false;
+        }
+    }
+    public static void ForceRefresh(ImportAssetOptions options)
+    {
+        if (isStartAssetEditing)
+            AssetDatabase.StopAssetEditing();
+        AssetDatabase.Refresh(options);
+        if (isStartAssetEditing)
+            AssetDatabase.StartAssetEditing();
+    }
+    public static string GetRandomNameByTimeStamp()
+    {
+        var timeSpan = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        var ramdomName = (long)timeSpan.TotalMilliseconds;
+        return ramdomName.ToString();
+    }
+}
+
